46e994be268a7355db96401524c28c88f69e66fe
Benjosss <b55.lallement@gmail.com>
2026-01-14 21:53:04 +0100

    Modified imports

--- src/sections/Navbar.jsx
+++ src/sections/Navbar.jsx
@@ -26,11 +26,11 @@ const Navbar = () => {
         <header className="fixed top-0 left-0 right-0 z-50 bg-black/90">
             <div className="mx-auto">
                 <div className="flex justify-between items-center py-5 mx-auto c-space">
-                    <a href="/"
+                    <Link to="/"
                         className="text-neutral-400 font-bold sm:text-2xl text-xl hover:text-white transition-colors"
                     >
                         Benjamin
-                    </a>
+                    </Link>
 
                     {/*Small devices*/}
                     <button onClick={toggleOpen} className="text-neutral-400 hover:text-white focus:outline-none sm:hidden flex z-30" aria-label="Toggle navigation">
