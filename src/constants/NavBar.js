e40d6b606d659b2c64db9a729662d24e2f25da8d
Benjosss <b55.lallement@gmail.com>
2026-01-14 21:30:53 +0100

    Responsive navbar and path modification

---
+++ src/constants/NavBar.js
@@ -0,0 +1,27 @@
+export const navBarElements = [
+    {
+        id: 1,
+        name: 'Home',
+        href: '#home'
+    },
+    {
+        id: 2,
+        name: 'Projects',
+        href: '#projects'
+    },
+    {
+        id: 3,
+        name: 'Skills',
+        href: '#skills'
+    },
+    {
+        id: 4,
+        name: 'About',
+        href: '#about'
+    },
+    {
+        id: 4,
+        name: 'Contact',
+        href: '#contact'
+    },
+]
--- src/sections/Navbar.jsx
+++ src/sections/Navbar.jsx
@@ -1,28 +1,16 @@
 import {useState} from 'react';
+import {navBarElements} from "../constants/NavBar.js";
 
 const NavItems = () => {
     return (
         <ul className="flex flex-col items-center gap-4 sm:flex-row md:gap-6 relative z-20">
-            <li className="text-neutral-400 hover:text-white font-generalsans max-sm:hover:bg-black-500 max-sm:w-full max-sm:rounded-md py-2 max-sm:px-5">
-                <a className="text-lg md:text-base hover:text-white transition-colors" href="#home">
-                    Home
-                </a>
-            </li>
-            <li className="text-neutral-400 hover:text-white font-generalsans max-sm:hover:bg-black-500 max-sm:w-full max-sm:rounded-md py-2 max-sm:px-5">
-                <a className="text-lg md:text-base hover:text-white transition-colors" href="#about">
-                    About
-                </a>
-            </li>
-            <li className="text-neutral-400 hover:text-white font-generalsans max-sm:hover:bg-black-500 max-sm:w-full max-sm:rounded-md py-2 max-sm:px-5">
-                <a className="text-lg md:text-base hover:text-white transition-colors" href="#work">
-                    Work
-                </a>
-            </li>
-            <li className="text-neutral-400 hover:text-white font-generalsans max-sm:hover:bg-black-500 max-sm:w-full max-sm:rounded-md py-2 max-sm:px-5">
-                <a className="text-lg md:text-base hover:text-white transition-colors" href="#contact">
-                    Contact
-                </a>
-            </li>
+            {navBarElements.map(({id, name, href}) => (
+                <li key={id} className="text-neutral-400 hover:text-white font-generalsans max-sm:hover:bg-black-500 max-sm:w-full max-sm:rounded-md py-2 max-sm:px-5">
+                    <a className="sm:text-xl text-lg md:text-base hover:text-white transition-colors" href={href}>
+                        {name}
+                    </a>
+                </li>
+            ))}
         </ul>
     )
 }
@@ -38,7 +26,7 @@ const Navbar = () => {
         <header className="fixed top-0 left-0 right-0 z-50 bg-black/90">
             <div className="mx-auto">
                 <div className="flex justify-between items-center py-5 mx-auto c-space">
-                    <a href="/" className="text-neutral-400 font-bold text-xl hover:text-white transition-colors">
+                    <a href="" className="text-neutral-400 font-bold sm:text-2xl text-xl hover:text-white transition-colors">
                         Benjamin
                     </a>
 
