14883206ad74dc2906fdc8346de4d02b3944555f
Benjosss <b55.lallement@gmail.com>
2026-01-18 10:11:04 +0100

    Added section title and anchor

--- src/sections/About.jsx
+++ src/sections/About.jsx
@@ -1,6 +1,7 @@
 const About = () => {
     return (
-        <section id="about" className="c-space my-20">
+        <section className="c-space my-20">
+            <h2 id="about" className="text-neutral-400 text-3xl font-bold mb-10 ml-3">About me</h2>
             <div className="grid h-full xl:grid-cols-3 xl:grid-rows-6 md:grid-cols-2 grid-cols-1 gap-5">
                 <div className="col-span-1 xl:row-span-3">
                     <div className="grid-container">
