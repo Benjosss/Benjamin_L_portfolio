5f5c352917e606dfe7172119afd344864f3bfcc6
Benjosss <b55.lallement@gmail.com>
2026-01-21 20:58:46 +0100

    Slider fill

---
+++ src/constants/SkillsConst.js
@@ -0,0 +1,39 @@
+export const skills = [
+
+    {
+        id: 1,
+        name: 'Backend',
+        elements : ["Java", "PHP", "Node.js"]
+    },
+    {
+        id: 2,
+        name: 'Frontend',
+        elements : ["HTML", "CSS", "Javascript"]
+    },
+    {
+        id: 3,
+        name: 'Data Science / IA',
+        elements : ["Python", "SQL", "PL/SQL"]
+    },
+    {
+        id: 4,
+        name: 'System',
+        elements : ["C", "C++", "Bash", "Git"]
+    },
+    {
+        id: 5,
+        name: 'Softwares',
+        elements : ["Github/GitLab", "Figma", "Docker","VSCode", "Visual Studio", "JetBrain IDEs", "Eclipse", "VirtualBox", "Cisco packet tracer", "Photoshop", "Premiere Pro"]
+    },
+    {
+        id: 6,
+        name: 'Soft Skills',
+        elements : ["Team working", "Rigor", "Organization", "Curiosity"]
+    },
+    {
+        id: 7,
+        name: 'Languages',
+        elements : ["French", "English", "Spanish", 'Arabic'],
+        levels: ['Native', 'Fluent', 'Academic', 'Basic']
+    }
+];
--- src/sections/Skills.jsx
+++ src/sections/Skills.jsx
@@ -1,9 +1,10 @@
 import Slider from "react-slick";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
+import {skills} from "../constants/SkillsConst.js"
 
 const Skills = () => {
-    var settings = {
+    const settings = {
         dots: true,
         infinite: true,
         speed: 500,
@@ -17,34 +18,18 @@ const Skills = () => {
         <section className="c-space my-20">
             <h2 id="skills" className="text-neutral-400 text-3xl font-bold mb-10 ml-3">Skills</h2>
             <Slider {...settings} className="w-full h-full font text-amber-50 " >
-                <div className="c-space grid-container gap-5">
-                    <div>
-                        <p className="grid-headtext">Tech skills</p>
-                        <p className="grid-subtext">
-                            During my three years of undergraduate study,
-                            I honed my skills in several areas of computer science,
-                            learned several <span className="font-semibold">programming languages</span>,
-                            and implemented several important <span className="font-semibold">algorithms</span> and
-                            <span className="font-semibold"> programming paradigms</span>.
-                        </p>
+                {skills.map(({name, elements}) => (
+                    <div className="c-space grid-container gap-5">
+                        <div key={name}>
+                            <p className="grid-headtext">{name}</p>
+                            <div className="grid-subtext">
+                                {elements.map((element, idx) => (
+                                    <p key={`${idx}-${element}`}>{element}</p>
+                                ))}
+                            </div>
+                        </div>
                     </div>
-                    <img src="icons/grid_1.png" className="w-full sm:h-[276px] h-fit object-contain mt-5"/>
-                </div>
-                <div>
-                    <img src="icons/grid_1.png" className="w-full sm:h-[276px] h-fit object-contain"/>
-                </div>
-                <div>
-                    <img src="icons/grid_1.png" className="w-full sm:h-[276px] h-fit object-contain"/>
-                </div>
-                <div>
-                    <img src="icons/grid_1.png" className="w-full sm:h-[276px] h-fit object-contain"/>
-                </div>
-                <div>
-                    <img src="icons/grid_1.png" className="w-full sm:h-[276px] h-fit object-contain"/>
-                </div>
-                <div>
-                    <img src="icons/grid_1.png" className="w-full sm:h-[276px] h-fit object-contain"/>
-                </div>
+                ))}
             </Slider>
         </section>
     )
