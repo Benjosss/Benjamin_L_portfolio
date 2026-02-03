3a7e5f0c374b72c8ff12c715bde0c6cfa0143f1d
Benjosss <b55.lallement@gmail.com>
2026-02-03 20:04:26 +0100

    Projects section begin

---
+++ src/constants/ProjectsConst.js
@@ -0,0 +1,35 @@
+export const projects = [
+
+    {
+        title: 'Uno game',
+        desc: 'Front and Back end of Uno game',
+        subdesc: 'Front and Back end of Uno game',
+        href: 'https://github.com/Benjosss/Personal_newsletter.git',
+        texture: '',
+        logo: 'icons/grid_1.png',
+        logoStyle: {
+            backgroundColor: '#2A1816',
+            border: '0.2px solid #36201D',
+            boxShadow: '0px 0px 60px 0px #AA3C304D',
+        },
+        tags: [
+            {
+                id: 1,
+                name: 'Java',
+                path: '/assets/react.svg',
+            },
+            {
+                id: 2,
+                name: 'JavaFX',
+                path: 'assets/tailwindcss.png',
+            },
+            {
+                id: 3,
+                name: 'MySQL',
+                path: '/assets/typescript.png',
+            },
+        ],
+
+    },
+
+];
--- src/sections/Projects.jsx
+++ src/sections/Projects.jsx
@@ -1,12 +1,66 @@
-import React from 'react'
+import {projects} from "../constants/ProjectsConst.js";
+import React, {Suspense, useState} from 'react'
+
+const projectsCount = projects.length;
 
 const Projects = () => {
+    const [selectedProjectIndex, setSelectedProjectIndex] = useState(0)
+
+    const currentProject = projects[selectedProjectIndex];
+
+    const handleNavigation = (direction) => {
+        setSelectedProjectIndex((prevIndex) => {
+            if(direction === "previous") {
+                return prevIndex === 0 ? projectsCount - 1 : prevIndex - 1;
+            } else{
+                return prevIndex === projectsCount - 1 ? 0 : prevIndex + 1  ;
+            }
+        })
+    }
+
     return (
         <section className="c-space my-20">
             <h2 id="projects" className="text-neutral-400 text-3xl font-bold mb-10 ml-3">My Projects</h2>
 
-        </section>
+            <div className="grid lg:grid-cols-2 grid-cols-1 mt-12 gap-5 w-full text-amber-50">
+                <div className="flex flex-col gap-5 relative sm:p-10 py-10 px-5 shadow-2xl shadow-black-200">
+                    <div className="p-3 backdrop-filter backdrop-blur-3xl w-fit rounded-lg" style={currentProject.logoStyle}>
+                        <img src={currentProject.logo} alt="logo" className="w-10 h-10 shadow-sm" />
+                    </div>
+                    <div className="flex flex-col gap-5 text-white-600 my-5">
+                        <p className="text-white text-2xl font-semibold animatedText">{currentProject.title}</p>
+                        <p className="animatedText">{currentProject.desc}</p>
+                        <p className="animatedText">{currentProject.subdesc}</p>
+                    </div>
+                    <div className="flex items-center justify-between flex-wrap gap-5">
+                        <div className="flex items-center gap-3">
+                            {currentProject.tags.map((tag, index) => (
+                                <div key={index} className="tech-logo">
+                                    <img src={tag.path} alt={tag.name} />
+                                </div>
+                            ))}
+                        </div>
+                        <a className="flex items-center gap-2 cursor-pointer text-white-600"
+                           href={currentProject.href} target="_blank" rel="noreferrer">
+                            <p>Check Live Site</p>
+                            <img src="/assets/arrow-up.png" alt="arrow" className="w-3  h-3" />
+                        </a>
+                    </div>
+                    <div className="flex justify-between items-center mt-7">
+                        <button className="arrow-btn" onClick={() => handleNavigation('previous')}>
+                            <img src="/assets/left-arrow.png" alt="left-arrow" className="w-4  h-4" />
+                        </button>
+                        <button className="arrow-btn" onClick={() => handleNavigation('next')}>
+                            <img src="/assets/right-arrow.png" alt="right-arrow" className="w-4  h-4" />
+                        </button>
+                    </div>
+                </div>
 
+                <div className="border border-black-300 bg-black-300 rounded-lg h-96 md:h-full">
+                    <p>3D Model</p>
+                </div>
+            </div>
+        </section>
     )
 }
 export default Projects
